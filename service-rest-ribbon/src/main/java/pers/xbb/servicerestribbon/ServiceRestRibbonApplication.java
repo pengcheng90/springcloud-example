724fc227222fc5a5367f8c9d6069732752bab76c
xiebaobiao <xiebaobiao>
2018-06-25 16:13:46 +0800

    add HystrixDashboard仪表盘

--- service-rest-ribbon/src/main/java/pers/xbb/servicerestribbon/ServiceRestRibbonApplication.java
+++ service-rest-ribbon/src/main/java/pers/xbb/servicerestribbon/ServiceRestRibbonApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -12,6 +13,8 @@ import org.springframework.web.client.RestTemplate;
 @EnableDiscoveryClient
 //断路器
 @EnableHystrix
+//开启HystrixDashboard仪表盘
+@EnableHystrixDashboard
 public class ServiceRestRibbonApplication {
 
     public static void main(String[] args) {
@@ -19,7 +22,7 @@ public class ServiceRestRibbonApplication {
     }
 
     @Bean
-//    @LoadBalanced开启ribbon负载均衡
+    //开启ribbon负载均衡
     @LoadBalanced
     RestTemplate restTemplate() {
         return new RestTemplate();
